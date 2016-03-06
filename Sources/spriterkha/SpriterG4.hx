2b8b29a232e0036fa47e4fdf29b5c80b4dc7e701
wighawag <wighawag@gmail.com>
2016-03-06 07:54:48 +0000

    add rotation and scale

--- Sources/spriterkha/SpriterG4.hx
+++ Sources/spriterkha/SpriterG4.hx
@@ -17,12 +17,19 @@ class SpriterG4 {
 	// 	return drawSpriter(vertexData,vertexSize)
 	// }
 	
-	public static function writeSpriter(vertexData : Float32Array, vertexStart : Int, vertexSize : Int, posStride : Int, texStride : Int, rotStride : Int, indexData : Array<Int>, indexStart : Int, vertexIndex : Int,  imageSheet : ImageSheet, entity : EntityInstance, x : Float, y : Float) : Int{
+	// public static function writeSpriter(vertexData : Float32Array, vertexStart : Int, vertexSize : Int, posStride : Int, texStride : Int, indexData : Array<Int>, indexStart : Int, vertexIndex : Int,  imageSheet : ImageSheet, entity : EntityInstance, x : Float, y : Float) : Int{
+	// 	return writeSpriterWithRotation(vertexData,vertexStart, vertexSize, posStride, texStride, -1, indexData, indexStart, vertexIndex, imageSheet, entity, x, y, 1, 1); //TODO 1 1 = scale
+	// }
+	
+	public static function writeSpriterWithRotation(vertexData : Float32Array, vertexStart : Int, vertexSize : Int, posStride : Int, texStride : Int, rotStride : Int, indexData : Array<Int>, indexStart : Int, vertexIndex : Int,  imageSheet : ImageSheet, entity : EntityInstance, x : Float, y : Float, toScaleX : Float, toScaleY : Float, toAngle : Float) : Int{
 		var sprites = entity.sprites;
 		var current = sprites.start;
 		var counter = 0;
 		var indexCounter = 0;
 		while (current < sprites.top){
+			
+			//get filename
+			
 			var folderId = sprites.folderId(current);
 			var fileId = sprites.fileId(current);
 			var filename = entity.getFilename(folderId, fileId);
@@ -35,6 +42,9 @@ class SpriterG4 {
 				continue;
 			}
 			
+			
+			//getting values from entity sprite + imageSheet
+			
 			var pivotX = sprites.pivotX(current);
 			var pivotY = sprites.pivotY(current);
 			var offsetX = subImage.offsetX;
@@ -44,8 +54,8 @@ class SpriterG4 {
 			var height = subImage.originalHeight;
 			var originX = pivotX * width - offsetX;
 			var originY = (1.0 - pivotY) * height - offsetY;
-			var locationX = sprites.x(current) + x ;
-			var locationY = -sprites.y(current) + y;
+			var locationX = sprites.x(current);
+			var locationY = -sprites.y(current);
 			var scaleX = sprites.scaleX(current); 
 			var scaleY = sprites.scaleY(current); 
 			var angle = -sprites.angle(current);
@@ -63,6 +73,21 @@ class SpriterG4 {
 				subHeight = subImage.width;
 			}
 			
+			
+			//applying arguments
+			locationX *= toScaleX;
+			locationY *= toScaleY;
+			var locX = locationX;
+			locationX = Math.cos(toAngle) * locationX - Math.sin(toAngle) * locationY;
+			locationY = Math.cos(toAngle) * locationY + Math.sin(toAngle) * locX;
+			locationX += x;
+			locationY += y;
+			scaleX *= toScaleX;
+			scaleY *= toScaleY;
+			angle += toAngle;
+			
+			
+			//computing each corner
 
 			var x = locationX;
 			var y = locationY;
@@ -88,30 +113,47 @@ class SpriterG4 {
 			var brX = x + (dx + w) * cos - (dy + h) * sin;
 			var brY = y + (dx + w) * sin + (dy + h) * cos;
 			
+			
+			
+			//writing vertex data
+			
 			vertexData.set(vertexStart+vertexSize*counter+posStride+0,tlX);
 			vertexData.set(vertexStart+vertexSize*counter+posStride+1,tlY);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+0,subImage.x / imageSheet.image.width);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+1,subImage.y / imageSheet.image.height);
-			vertexData.set(vertexStart+vertexSize*counter+rotStride,-angle);
+			if(rotStride > 0){
+				vertexData.set(vertexStart+vertexSize*counter+rotStride,-angle);
+			}
+			
 			
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*1+0,trX);
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*1+1,trY);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*1+0,(subImage.x + subWidth) / imageSheet.image.width);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*1+1,subImage.y / imageSheet.image.height);
-			vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*1,-angle);
+			if(rotStride > 0){
+				vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*1,-angle);
+			}
 			
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*2+0,blX);
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*2+1,blY);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*2+0,subImage.x / imageSheet.image.width );
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*2+1,(subImage.y + subHeight) / imageSheet.image.height);
-			vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*2,-angle);
+			if(rotStride > 0){
+				vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*2,-angle);
+			}
 			
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*3+0,brX);
 			vertexData.set(vertexStart+vertexSize*counter+posStride+vertexSize*3+1,brY);
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*3+0,(subImage.x + subWidth) / imageSheet.image.width );
 			vertexData.set(vertexStart+vertexSize*counter+texStride+vertexSize*3+1,(subImage.y + subHeight) / imageSheet.image.height);
-			vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*3,-angle);
+			if(rotStride > 0){
+				vertexData.set(vertexStart+vertexSize*counter+rotStride+vertexSize*3,-angle);
+			}
+			
 			counter +=4;
+			
+			
+			//writing index data
 			
 			indexData[indexStart+indexCounter+0] = vertexIndex+0;
 			indexData[indexStart+indexCounter+1] = vertexIndex+1;
